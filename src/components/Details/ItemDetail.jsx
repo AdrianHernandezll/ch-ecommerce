5f1e03a647e7be81f60eddec19c0987aae7b8b1d
danux99 <danix08@live.com>
2021-10-13 00:22:01 -0300

    borrando comentarios

--- src/components/Details/ItemDetail.jsx
+++ src/components/Details/ItemDetail.jsx
@@ -62,41 +62,6 @@ const ItemDetail = ({ object }) => {
 
         </>
 
-        // <Grid container justifyContent="center" >
-        //     <Grid item lg={4} md={8} sm={8} xs={12} >
-        //         <Card className={classes.root}>
-        //             <CardMedia className={classes.media} image={object.image} title={object.name} />
-
-        //         </Card>
-        //     </Grid>
-        //     <Grid container lg={4} md={8} sm={8} xs={12}>
-        //         <Card className={classes.root}>
-        //             <CardContent >
-        //                 <div className={classes.cardContent}>
-        //                     <Typography variant="h5" gutterBottom>
-        //                         {object.name}
-        //                     </Typography>
-        //                     <Typography variant="h5">
-        //                         ${object.price}
-        //                     </Typography>
-
-        //                 </div>
-        //                 <Typography variant="body2">{object.description}</Typography>
-        //             </CardContent>
-        //             <Grid container >
-        //                 <Grid item md={10} sx={{ flexGrow: 1 }} >
-        //                     <CardActions disableSpacing className={classes.cardActions}>
-        //                         <ItemCount stock={5} initial={1} onAdd={onAdd} />
-        //                     </CardActions>
-        //                 </Grid>
-
-        //             </Grid>
-        //         </Card>
-        //     </Grid>
-        // </Grid >
-
-
-
 
     )
 }
