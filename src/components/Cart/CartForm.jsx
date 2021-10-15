d30ca84abca2902df3970f144d97ac5768aaef34
danux99 <danix08@live.com>
2021-10-15 13:17:26 -0300

    agregando CartForm

--- src/components/Cart/CartForm.jsx
+++ src/components/Cart/CartForm.jsx
@@ -2,9 +2,42 @@ import React from 'react'
 
 const CartForm = () => {
     return (
-        <div>
+        <form onSubmit={handleOnSubmit}>
+            <input
+                type='text'
+                placeholder='ingrese el nombre'
+                name='name'
+                value={formData.name}
+                onChange={handleOnChange}
 
-        </div>
+            />
+            <input
+                type='text'
+                placeholder='ingrese el nro de tel'
+                name='tel'
+                value={formData.tel}
+                onChange={handleOnChange}
+
+            />
+            <input
+                type='text'
+                placeholder='ingrese el email'
+                name='email'
+                value={formData.email}
+                onChange={handleOnChange}
+
+            />
+            <input
+                type='text'
+                placeholder='Confirme el mail '
+                name='email2'
+                value={formData.email}
+                onChange={handleOnChange}
+            />
+
+            <Button type="submit" className="btn btn-out btn-primary btn-square btn-main" data-abc="true" onClick={notify} > Realizar Compra </Button>
+
+        </form>
     )
 }
 
