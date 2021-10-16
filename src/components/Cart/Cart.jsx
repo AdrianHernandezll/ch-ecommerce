a4c9cccb57a867744ebbefbed831eed572650c7d
danux99 <danix08@live.com>
2021-10-15 21:58:14 -0300

    creando CartForm

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,5 +1,4 @@
-import { useState } from 'react';
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Container from 'react-bootstrap/Container'
 import { useCartContext } from '../../Context/CartContext';
@@ -9,8 +8,6 @@ import Button from 'react-bootstrap/Button'
 import { Row } from 'react-bootstrap';
 import Table from 'react-bootstrap/Table';
 import { LinkContainer } from 'react-router-bootstrap';
-import { getFirestore } from '../../services/getFirebase';
-import firebase from 'firebase/app'
 import 'firebase/firestore'
 
 
@@ -20,91 +17,6 @@ const Cart = () => {
 
     const { cartList, removeItem, totalPrice, addOneItem, removeOneItem, clear } = useCartContext();
     const isInCart = !cartList.length;
-
-    const notify = () =>
-        setTimeout(() => {
-            toast("Orden Realizada Correctamente")
-            setTimeout(() => {
-                clear()
-            })
-        }, 3000);;
-
-    const [formData, setFormData] = useState({
-        name: '',
-        tel: '',
-        email: ''
-    })
-    const handleOnSubmit = (e) => {
-        e.preventDefault()
-        let orden = {}
-
-        orden.date = firebase.firestore.Timestamp.fromDate(new Date());
-
-        orden.buyer = formData;
-
-        orden.total = totalPrice();
-
-        orden.items = cartList.map(cartItem => {
-            const id = cartItem.object.id;
-            const title = cartItem.object.title;
-            const price = cartItem.object.price * cartItem.quantity;
-
-            return { id, title, price }
-        })
-
-
-        const db = getFirestore()
-        db.collection('orders').add(orden)
-            .then(resp => console.log(resp.id))
-            .catch(err => console.log(err))
-            .finally(() =>
-                setFormData({
-                    name: '',
-                    tel: '',
-                    email: ''
-                })
-                //borrarLista()
-            )
-
-
-        //Actualiza todos los items que estan en el listado de Cart del CartContext
-        const itemsToUpdate = db.collection('items').where(
-            firebase.firestore.FieldPath.documentId(), 'in', cartList.map(i => i.object.id)
-        )
-
-        const batch = db.batch();
-
-        // por cada item restar del stock la cantidad de el carrito
-
-        itemsToUpdate.get()
-            .then(collection => {
-                collection.docs.forEach(docSnapshot => {
-                    batch.update(docSnapshot.ref, {
-                        stock: docSnapshot.data().stock - cartList.find(item => item.object.id === docSnapshot.id).quantity
-                    })
-                })
-
-                batch.commit().then(res => {
-                    console.log('resultado batch:', res)
-                })
-            })
-
-
-    }
-
-
-    function handleOnChange(e) {
-
-        setFormData({
-            ...formData,
-            [e.target.name]: e.target.value
-        })
-
-
-    }
-
-
-
 
     const EmptyCard = () => (
         <Container>
@@ -117,7 +29,6 @@ const Cart = () => {
 
                 </LinkContainer>
             </Col>
-
         </Container>
     )
 
@@ -162,45 +73,6 @@ const Cart = () => {
                 ))}
 
             </Col>
-
-            <form onSubmit={handleOnSubmit}>
-                <input
-                    type='text'
-                    placeholder='ingrese el nombre'
-                    name='name'
-                    value={formData.name}
-                    onChange={handleOnChange}
-
-                />
-                <input
-                    type='text'
-                    placeholder='ingrese el nro de tel'
-                    name='tel'
-                    value={formData.tel}
-                    onChange={handleOnChange}
-
-                />
-                <input
-                    type='text'
-                    placeholder='ingrese el email'
-                    name='email'
-                    value={formData.email}
-                    onChange={handleOnChange}
-
-                />
-                <input
-                    type='text'
-                    placeholder='Confirme el mail '
-                    name='email2'
-                    value={formData.email}
-                    onChange={handleOnChange}
-                />
-
-                <Button type="submit" className="btn btn-out btn-primary btn-square btn-main" data-abc="true" onClick={notify} > Realizar Compra </Button>
-
-            </form>
-
-
         </>
     )
     return (
--- src/components/Cart/CartForm.jsx
+++ src/components/Cart/CartForm.jsx
@@ -1,6 +1,92 @@
-import React from 'react'
+import { useState } from 'react';
+import { getFirestore } from '../../services/getFirebase';
+import firebase from 'firebase/app';
+import { toast } from 'react-toastify';
+
 
 const CartForm = () => {
+    const notify = () =>
+        setTimeout(() => {
+            toast("Orden Realizada Correctamente")
+            setTimeout(() => {
+                clear()
+            })
+        }, 3000);;
+
+    const [formData, setFormData] = useState({
+        name: '',
+        tel: '',
+        email: ''
+    })
+    const handleOnSubmit = (e) => {
+        e.preventDefault()
+        let orden = {}
+
+        orden.date = firebase.firestore.Timestamp.fromDate(new Date());
+
+        orden.buyer = formData;
+
+        orden.total = totalPrice();
+
+        orden.items = cartList.map(cartItem => {
+            const id = cartItem.object.id;
+            const title = cartItem.object.title;
+            const price = cartItem.object.price * cartItem.quantity;
+
+            return { id, title, price }
+        })
+
+
+        const db = getFirestore()
+        db.collection('orders').add(orden)
+            .then(resp => console.log(resp.id))
+            .catch(err => console.log(err))
+            .finally(() =>
+                setFormData({
+                    name: '',
+                    tel: '',
+                    email: ''
+                })
+                //borrarLista()
+            )
+
+
+        //Actualiza todos los items que estan en el listado de Cart del CartContext
+        const itemsToUpdate = db.collection('items').where(
+            firebase.firestore.FieldPath.documentId(), 'in', cartList.map(i => i.object.id)
+        )
+
+        const batch = db.batch();
+
+        // por cada item restar del stock la cantidad de el carrito
+
+        itemsToUpdate.get()
+            .then(collection => {
+                collection.docs.forEach(docSnapshot => {
+                    batch.update(docSnapshot.ref, {
+                        stock: docSnapshot.data().stock - cartList.find(item => item.object.id === docSnapshot.id).quantity
+                    })
+                })
+
+                batch.commit().then(res => {
+                    console.log('resultado batch:', res)
+                })
+            })
+
+
+    }
+
+
+    function handleOnChange(e) {
+
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        })
+
+
+    }
+
     return (
         <form onSubmit={handleOnSubmit}>
             <input
@@ -34,9 +120,7 @@ const CartForm = () => {
                 value={formData.email}
                 onChange={handleOnChange}
             />
-
             <Button type="submit" className="btn btn-out btn-primary btn-square btn-main" data-abc="true" onClick={notify} > Realizar Compra </Button>
-
         </form>
     )
 }
